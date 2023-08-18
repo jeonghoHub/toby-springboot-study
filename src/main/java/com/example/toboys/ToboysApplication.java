ec473cbad204edff2e01913a7c0d6fc937167106
jeonghoHub <jeonghoHub>
2023-08-18 20:47:28 +0900

    :sparkles: init

---
+++ src/main/java/com/example/toboys/ToboysApplication.java
@@ -0,0 +1,13 @@
+package com.example.toboys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToboysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToboysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toboys/ToboysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toboys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToboysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
