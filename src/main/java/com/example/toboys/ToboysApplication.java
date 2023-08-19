f54cee551394ec4907d9c5d0fed3e1108dece709
jeonghoHub <jeonghoHub>
2023-08-20 00:34:34 +0900

    :sparkles: TomcatServletWebServerFactor를 사용하여 톰캣 웹 서버(서블릿 컨테이너) 띄우기

--- src/main/java/com/example/toboys/ToboysApplication.java
+++ src/main/java/com/example/toboys/ToboysApplication.java
@@ -1,13 +1,15 @@
 package com.example.toboys;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class ToboysApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ToboysApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
