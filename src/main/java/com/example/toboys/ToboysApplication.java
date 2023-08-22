7781f2e25af2070dd65bbcd9ae3839818b42121a
jeonghoHub <tjsrsdqq4@gmail.com>
2023-08-22 22:23:05 +0900

    :sparkles: 컨트롤러 매핑 및 바인딩 처리

--- src/main/java/com/example/toboys/ToboysApplication.java
+++ src/main/java/com/example/toboys/ToboysApplication.java
@@ -21,6 +21,7 @@ public class ToboysApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -28,10 +29,11 @@ public class ToboysApplication {
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
-                        resp.setStatus(HttpStatus.OK.value());
+                        String ret = helloController.hello(name);
 
+                        resp.setStatus(HttpStatus.OK.value());
                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println("Hello " + name);
+                        resp.getWriter().println(ret);
                     } else if (req.getRequestURI().equals("/user")) {
                         //
                     } else {
